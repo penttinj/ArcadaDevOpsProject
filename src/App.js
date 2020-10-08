32a8ee3f4342376212166841ccb0624d96cb212d
Johan Penttinen <johanlepenttinen@gmail.com>
2020-10-08 19:06:33 +0300

    Add button to fetch users from api and renders them

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import UserList from "./components/UserList";
 import './App.css';
 
+
+
+
+
 function App() {
+  const [clicked, setClicked] = useState(null);
+  const [users, setUsers] = useState("null");
+
+  async function getUsers() {
+    const response = await fetch("https://8pipgsuihc.execute-api.eu-central-1.amazonaws.com/users")
+      .catch(e => console.log(`Couldn't fetch users: ${e}`));
+    console.log("response", response);
+    const json = await response.json();
+    console.log("json:", json);
+    setUsers(json);
+  }
+
+  function handleClick() {
+    setClicked(!clicked);
+    getUsers();
+  }
+
+  // Button for getting users, one for show/hide
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="layout">
+        <header className="App-header">
+
+        </header>
+        <div className="ButtonBanner">
+          <button className="Button" onClick={handleClick} className="Button">
+            {clicked ? "Hide Users" : "Get Users"}
+          </button>
+        </div>
+        <div className="UserListBanner">
+          {clicked ? <UserList data={users} /> : ""}
+        </div>
+      </div>
     </div>
   );
 }
